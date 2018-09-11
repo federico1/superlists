4ea364eb1a6adf5592a67b787c68a398b647aaf1
federico1 <federico.roca@yahoo.com>
2018-09-11 11:56:09 -0400

    Update settings.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm_*#^bd@^6=#_1p#72yccx%%!_)@$r72uc9v0#(3j=k*t_6&id'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
